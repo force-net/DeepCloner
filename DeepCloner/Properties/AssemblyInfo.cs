165798eb37066846d77d236e8fa72548e9b2d374
Sverre Sørensen <sverre.sorensen@evry.com>
2019-02-26 09:38:06 +0100

    Pbi #121370 - Moving to Azure DevOps

--- DeepCloner/Properties/AssemblyInfo.cs
+++ DeepCloner/Properties/AssemblyInfo.cs
@@ -4,13 +4,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("EVRY.DeepCloner")]
-[assembly: AssemblyDescription("Library for cloning objects")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("EVRY")]
-[assembly: AssemblyProduct("DeepCloner")]
-[assembly: AssemblyCopyright("Copyright � Force 2016-2017")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("Copyright © Force 2016-2017")]
+[assembly: AssemblyTrademark("Elements™")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -20,17 +15,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("2e4d0d08-2858-4797-bb4c-9b700601a8c0")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-
-[assembly: AssemblyVersion("0.10.0.0")] // change this value only when api is changing
-[assembly: AssemblyFileVersion("0.10.2.0")]
-[assembly: AssemblyInformationalVersion("0.10.2.0")]
