ca94050761693504b101576a63e6ebf31803d002
smolchanovsky <smolchanovsky@gmail.com>
2019-12-22 11:34:16 +0500

    add interface ICloner

--- DeepCloner/Cloner.cs
+++ DeepCloner/Cloner.cs
@@ -3,9 +3,34 @@
 namespace Force.DeepCloner
 {
 	/// <summary>
+	/// Interface for classes capable to perform copy of object
+	/// </summary>
+	public interface ICloner
+	{
+		/// <summary>
+		/// Performs deep (full) copy of object and related graph
+		/// </summary>
+		T DeepClone<T>(T obj);
+
+		/// <summary>
+		/// Performs deep (full) copy of object and related graph to existing object
+		/// </summary>
+		/// <returns>existing filled object</returns>
+		/// <remarks>Method is valid only for classes, classes should be descendants in reality, not in declaration</remarks>
+		TTo DeepCloneTo<TFrom, TTo>(TFrom objFrom, TTo objTo) where TTo : class, TFrom;
+
+		/// <summary>
+		/// Performs shallow copy of object to existing object
+		/// </summary>
+		/// <returns>existing filled object</returns>
+		/// <remarks>Method is valid only for classes, classes should be descendants in reality, not in declaration</remarks>
+		TTo ShallowCloneTo<TFrom, TTo>(TFrom objFrom, TTo objTo) where TTo : class, TFrom;
+	}
+
+	/// <summary>
 	/// Performs copy of object
 	/// </summary>
-	public class Cloner
+	public class Cloner : ICloner
 	{
 		public Cloner()
 		{
